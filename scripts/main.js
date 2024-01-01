b94c89d193b61bfc5346fe33245aae5788602b5f
Dukhi Atma <140259952+Dukh14tma@users.noreply.github.com>
2024-01-01 09:53:49 +0530

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
-
+function readMore(btn) {
+    const post = btn.closest('.post');
+    const dots = post.querySelector('.dots');
+    const moreContent = post.querySelector('.more-read');
+    if (dots.style.display==='none') {
+        dots.style.display='inline';
+        moreContent.style.display='none';
+        btn.innerHTML = 'Read More';
+    } else {
+        dots.style.display='none';
+        moreContent.style.display='inline';
+        btn.innerHTML = 'Read Less';
+    }
+}
